c9f7c6aa58c10a883ecd1daf40129db7486e8513
Akira Otaka <hydrangeas.project@gmail.com>
2019-11-07 23:42:34 +0900

    MainWindowViewModelへのテスト追加

--- CadFilerTest.Tests/MainWindowViewModelTest.cs
+++ CadFilerTest.Tests/MainWindowViewModelTest.cs
@@ -1,4 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
 
 namespace CadFilerTest.Tests
 {
@@ -8,6 +11,19 @@ namespace CadFilerTest.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            var cadFileMock = new Mock<ICadFileRepository>();
+            var entities = new List<CadFileEntity>();
+            entities.Add(
+                new CadFileEntity(
+                    "test.stl",
+                    new Guid("E93ECBD8-EB7F-4478-B99D-C1933EBA3563"),
+                    1024,
+                    1,
+                    Convert.ToDateTime("2019/11/07 23:45"),
+                    Convert.ToDateTime("2019/11/07 23:45")
+                ));
+            var viewModel = new MainWindowViewModel(cadFileMock.Object);
+            viewModel.CadFiles.Count.Is(2);
         }
     }
 }
