1fd840024c8ce3dc6d2c1d0da31dd67b790d29c3
Akira Otaka <hydrangeas.project@gmail.com>
2019-11-07 23:54:50 +0900

    エンティティ、リポジトリインタフェースを追加

---
+++ CadFile.Domain/Entities/CadFileEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadFile.Domain.Entities
+{
+    public sealed class CadFileEntity
+    {
+        public CadFileEntity(string logicalFileName,
+                             Guid physicalFileName,
+                             long fileSize,
+                             int displayOrder,
+                             DateTime created,
+                             DateTime updated)
+        {
+            LogicalFileName = logicalFileName;
+            PhysicalFileName = physicalFileName;
+            fileSize = FileSize;
+            displayOrder = DisplayOrder;
+            Created = created;
+            Updated = updated;
+        }
+
+        public string LogicalFileName { get; }
+        public Guid PhysicalFileName { get; }
+        public long FileSize { get; }
+        public int DisplayOrder { get; }
+        public DateTime Created { get; }
+        public DateTime Updated { get; }
+    }
+}
---
+++ CadFile.Domain/Repositories/ICadFileRepository.cs
@@ -0,0 +1,12 @@
+﻿using CadFile.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadFile.Domain.Repositories
+{
+    public interface ICadFileRepository
+    {
+        IReadOnlyList<CadFileEntity> GetData();
+    }
+}
