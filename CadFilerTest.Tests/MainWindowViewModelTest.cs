f582e139069e3ab3d878a5f6c8ed3f02ea5c84f3
Akira Otaka <hydrangeas.project@gmail.com>
2019-11-07 23:31:04 +0900

    テストプロジェクト追加

---
+++ CadFilerTest.Tests/MainWindowViewModelTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CadFilerTest.Tests
+{
+    [TestClass]
+    public class MainWindowViewModelTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
