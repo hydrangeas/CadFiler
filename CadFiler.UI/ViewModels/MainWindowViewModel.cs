7870de2e8627ab428473cdc1ee699448f40da3b9
Akira Otaka <hydrangeas.project@gmail.com>
2019-11-10 21:45:48 +0900

    D&D処理を仮実装

--- CadFiler.UI/ViewModels/MainWindowViewModel.cs
+++ CadFiler.UI/ViewModels/MainWindowViewModel.cs
@@ -1,24 +1,27 @@
 ﻿using CadFile.Domain.Repositories;
 using CadFiler.Infrastructure.LocalDB;
+using GongSolutions.Wpf.DragDrop;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
+using System.Windows;
 
 namespace CadFiler.UI.ViewModels
 {
-    public class MainWindowViewModel : ViewModelBase
+    public class MainWindowViewModel : ViewModelBase, IDropTarget
     {
         private ICadFileMetadataRepository _cadFile;
         public MainWindowViewModel()
-            :this(new CadFiles())
+            : this(new CadFiles())
         {
 
         }
         public MainWindowViewModel(ICadFileMetadataRepository cadFile)
         {
             _cadFile = cadFile;
-            foreach(var entity in _cadFile.GetData())
+            foreach (var entity in _cadFile.GetData())
             {
                 CadFiles.Add(new MainWindowViewModelCadFile(entity));
             }
@@ -27,9 +30,16 @@ namespace CadFiler.UI.ViewModels
         public ObservableCollection<MainWindowViewModelCadFile> CadFiles
         { get; set; } = new ObservableCollection<MainWindowViewModelCadFile>();
 
-        public void Save()
+        public void DragOver(IDropInfo dropInfo) => dropInfo.Effects = DragDropEffects.Copy;
+
+        public void Drop(IDropInfo dropInfo)
         {
-            throw new NotImplementedException();
+            dropInfo.Effects = DragDropEffects.Copy;
+            var dragFileList = ((DataObject)dropInfo.Data).GetFileDropList().Cast<string>();
+            foreach(var file in dragFileList)
+            {
+                System.Diagnostics.Debug.WriteLine($"{file}");
+            }
         }
     }
 }
