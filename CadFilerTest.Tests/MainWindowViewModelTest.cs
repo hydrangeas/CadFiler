7ff7f5d6c298a78e1b80fc9d49f3b157739c51f7
Akira Otaka <hydrangeas.project@gmail.com>
2019-11-10 17:39:07 +0900

    テストを追加、変更（Created/Updateが同じデータを使っていたので判別できるように）

--- CadFilerTest.Tests/MainWindowViewModelTest.cs
+++ CadFilerTest.Tests/MainWindowViewModelTest.cs
@@ -24,6 +24,49 @@ namespace CadFilerTest.Tests
                     1024,
                     1,
                     Convert.ToDateTime("2019/11/07 23:45"),
+                    Convert.ToDateTime("2019/11/07 23:46")
+                ));
+            entities.Add(
+                new CadFileEntity(
+                    "abc.stl",
+                    new Guid("8D3B5BE6-EF75-4FA4-9D9A-FCAA9D8875C1"),
+                    512,
+                    2,
+                    Convert.ToDateTime("2019/11/10 16:24"),
+                    Convert.ToDateTime("2019/11/10 16:25")
+                ));
+            cadFileMock.Setup(x => x.GetData()).Returns(entities);
+
+            var viewModel = new MainWindowViewModel(cadFileMock.Object);
+            viewModel.CadFiles.Count.Is(2);
+
+            viewModel.CadFiles[0].LogicalFileName.Is("test.stl");
+            viewModel.CadFiles[0].PhysicalFileName.Is(new Guid("E93ECBD8-EB7F-4478-B99D-C1933EBA3563"));
+            viewModel.CadFiles[0].FileSize.Is(1024);
+            viewModel.CadFiles[0].DisplayOrder.Is(1);
+            viewModel.CadFiles[0].Created.Is(Convert.ToDateTime("2019/11/07 23:45"));
+            viewModel.CadFiles[0].Updated.Is(Convert.ToDateTime("2019/11/07 23:46"));
+
+            viewModel.CadFiles[1].LogicalFileName.Is("abc.stl");
+            viewModel.CadFiles[1].PhysicalFileName.Is(new Guid("8D3B5BE6-EF75-4FA4-9D9A-FCAA9D8875C1"));
+            viewModel.CadFiles[1].FileSize.Is(512);
+            viewModel.CadFiles[1].DisplayOrder.Is(2);
+            viewModel.CadFiles[1].Created.Is(Convert.ToDateTime("2019/11/10 16:24"));
+            viewModel.CadFiles[1].Updated.Is(Convert.ToDateTime("2019/11/10 16:25"));
+        }
+
+        [TestMethod]
+        public void ファイルドロップ()
+        {
+            var cadFileMock = new Mock<ICadFileRepository>();
+            var entities = new List<CadFileEntity>();
+            entities.Add(
+                new CadFileEntity(
+                    "test.stl",
+                    new Guid("E93ECBD8-EB7F-4478-B99D-C1933EBA3563"),
+                    1024,
+                    1,
+                    Convert.ToDateTime("2019/11/07 23:45"),
                     Convert.ToDateTime("2019/11/07 23:45")
                 ));
             entities.Add(
@@ -40,12 +83,7 @@ namespace CadFilerTest.Tests
             var viewModel = new MainWindowViewModel(cadFileMock.Object);
             viewModel.CadFiles.Count.Is(2);
 
-            viewModel.CadFiles[0].LogicalFileName.Is("test.stl");
-            viewModel.CadFiles[0].PhysicalFileName.Is(new Guid("E93ECBD8-EB7F-4478-B99D-C1933EBA3563"));
-            viewModel.CadFiles[0].FileSize.Is(1024);
-            viewModel.CadFiles[0].DisplayOrder.Is(1);
-            viewModel.CadFiles[0].Created.Is(Convert.ToDateTime("2019/11/07 23:45"));
-            viewModel.CadFiles[0].Updated.Is(Convert.ToDateTime("2019/11/07 23:45"));
+
         }
     }
 }
